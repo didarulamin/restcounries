b7c624041991cb725a7f1ad09d47824497a10e25
Didarul Amin <programmer.didarul@gmail.com>
2022-04-07 02:00:18 +0600

    weather issue solve

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,5 +1,6 @@
-import { Container } from "@material-ui/core";
+import { Container, Paper } from "@material-ui/core";
 import { Box } from "@mui/system";
+
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -9,7 +10,7 @@ export default function Weather() {
 
   useEffect(() => {
     fetch(
-      `http://api.weatherstack.com/current?access_ke=30835b6e912e03bb4773bff87f6eff5b=${capital}`
+      `http://api.weatherstack.com/current?access_key=726f4b436038e3f96b726a4fd3e3a0a2&query=${capital}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -28,17 +29,31 @@ export default function Weather() {
     <Container>
       <Box
         sx={{
-          width: 300,
-          height: 300,
           padding: "1rem",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <h1>Weather of {capital}</h1>
-        <p>weather icon :</p>
-        <img src={weather?.current?.weather_icons[0]} alt="" />
-        <p>Temperature : {weather?.current?.temperature}</p>
-        <p>Wind Speed : {weather?.current?.wind_speed}</p>
-        <p>precip : {weather?.current?.precip}</p>
+        <Paper>
+          <Box
+            sx={{
+              padding: "1rem",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h1>Weather of {capital}</h1>
+            <p>Weather icon :</p>
+            <img src={weather?.current?.weather_icons[0]} alt="" />
+            <p>Temperature : {weather?.current?.temperature}</p>
+            <p>Wind Speed : {weather?.current?.wind_speed}</p>
+            <p>precip : {weather?.current?.precip}</p>
+          </Box>
+        </Paper>
       </Box>
     </Container>
   );
